738ceb5078e7121eb033ebda16ebb54f8af919ce
Mark <mlang41110@yahoo.com>
2017-03-18 15:47:21 -0400

    fixed phrase

--- HarrisWittelsQuoteRecursor.js
+++ HarrisWittelsQuoteRecursor.js
@@ -1,12 +1,11 @@
-const phraseWithBut = "I've said it before, but I'll say it again";
-const phraseWithAnd = phraseWithBut.replace(', but', ' and');
+const outerPhrase = "I've said it before, but I'll say it again";
+const nestedPhrase = outerPhrase.replace(', but', ' and');
 const numTimes = process.argv[2];
+
 const goDeeper = (phrase, numTimes) => {
-    phrase = phrase.replace(/it/g, () => {
-        return numTimes > 1 ? phraseWithBut : phraseWithAnd
-    });
+    phrase = phrase.replace(/it/g, nestedPhrase);
     numTimes--;
     return numTimes > 0 ? goDeeper(phrase, numTimes) : phrase;
 }
 
-console.log(`${goDeeper(phraseWithBut, numTimes)}.`);
+console.log(`${goDeeper(outerPhrase, numTimes)}.`);
